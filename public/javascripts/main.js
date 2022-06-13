c8fada315cc58102908de2c96f146ae7f975d7ca
Phan Hiền <phanhien203@gmail.com>
2022-06-14 01:15:46 +0700

    change title

---
+++ public/javascripts/main.js
@@ -0,0 +1,20 @@
+import 'bootstrap-4.2.1.js';
+import 'bootstrap-datetimepicker.min.js';
+
+import 'bootstrap4-toggle.min.js';
+import 'chart@2.9.0.js';
+import 'chartjs-plugin-annotation.js';
+import 'chartjs-plugin-annotation.min.js';
+import 'chartjs-plugin-streaming@1.8.0.js';
+import 'clockpicker.js';
+import 'fontawesome.js';
+import 'gijgo.min.js';
+import 'jquery-3.3.1.min.js';
+import 'json2html.min.js';
+import 'moment.min.js';
+import 'popper.min.js';
+import 'socket.io.js';
+import 'solid.js';
+import 'vue.js';
+
+console.log('main js')
