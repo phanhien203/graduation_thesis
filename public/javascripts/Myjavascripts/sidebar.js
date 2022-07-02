cbc68bf6edac6bfdf90520bf8b8696d423764175
Phan Hiền <phanhien203@gmail.com>
2022-07-02 14:02:35 +0700

    responsive

--- public/javascripts/Myjavascripts/sidebar.js
+++ public/javascripts/Myjavascripts/sidebar.js
@@ -1,4 +1,5 @@
 let expandButton = null;
+let hideButton = null;
 function clickExpand() {
     if (expandButton) {
         $('#sidebar').css({ "width": "250px" });
@@ -10,6 +11,22 @@ function clickExpand() {
         $('.menu-item> p, .narrow-sidebar').css({ "display": "none" });
         $('.expand-sidebar').css({ "border": "1px solid #fff", "transform": "rotate(180deg)"  });
         expandButton = !expandButton;
+    }
+}
+
+function clickHideSide() {
+    if (hideButton) {
+        $('#sidebar').css({ "margin-left": "-250px" });
+        $('.overlay, .close-button').css({ "display": "none"});
+        $('.hide-nav-bar').css({ "position": "relative", "left": "0px"});
+        $('.hide-button').css({ "display": "block"});
+        hideButton = !hideButton;
+    } else {
+        $('#sidebar').css({ "margin-left": "0px" });
+        $('.overlay, .close-button').css({  "display": "block"});
+        $('.hide-nav-bar').css({ "position": "fixed", "z-index": "111", "left": "210px"});
+        $('.hide-button').css({ "display": "none"});
+        hideButton = !hideButton;
     }
 }
 
